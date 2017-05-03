d1e832083297676a6d66bc443696b90f11dcae3a
tangliang <tangliang@haoju.cn>
2017-05-03 17:18:18 +0800

    code

--- example4.22.c
+++ example4.22.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
-
+static void myftw(char *);
+static void statistic(char *pathname, int length);
 static void fcategorise(char *filename, const unsigned int st_mode);
 
 static int n_rfile;  // number of regular file count.
@@ -11,18 +12,81 @@ static int n_pfile;  // number of named pipe count.
 static int n_sfile;  // number of socket count.
 static int n_lfile;  // number of symbolic link count.
 
+
 int main(int argc, char *argv[])
 {
-    if (argv != 2)
+    if (argc != 2)
         err_quit("Usage: %s <pathname>", argv[0]);
+
+    myftw(argv[1]);
+
+    printf("n_rfile: %d\n", n_rfile);
+    printf("n_dfile: %d\n", n_dfile);
+    printf("n_bfile: %d\n", n_bfile);
+    printf("n_cfile: %d\n", n_cfile);
+    printf("n_pfile: %d\n", n_pfile);
+    printf("n_sfile: %d\n", n_sfile);
+    printf("n_lfile: %d\n", n_lfile);
+    printf("  total: %d\n", n_rfile + n_dfile + n_bfile + n_cfile + n_pfile + n_sfile + n_lfile);
 }
 
 
-static 
+static size_t pathmax;
+static char *pathnamebuf;
 
-static void myFtw(char *pathname)
+
+static void myftw(char *pathname)
 {
+    pathnamebuf = path_alloc(&pathmax);
+    strncpy(pathnamebuf, pathname, pathmax);
+    strcat(pathnamebuf, "\0");
     
+    statistic(pathnamebuf, strlen(pathnamebuf));
+}
+
+
+/**
+ * Recursive dealing file and directory. 
+ */
+static void statistic(char *pathname, int length)
+{
+    DIR *dp;
+    char *ptr;
+    struct stat stbuf;
+    struct dirent *dir;
+    
+    if (lstat(pathname, &stbuf) < 0)
+        err_sys("lstat for %s error", pathname);
+
+    if (!S_ISDIR(stbuf.st_mode))
+        return;
+
+    if ((dp = opendir(pathname)) == NULL)
+        err_sys("opendir path %s error", pathname);
+
+    /* read dir entries */
+    while ((dir = readdir(dp)) != NULL) {
+        if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
+            continue;
+
+        ptr = pathname + length + 1;
+
+        *ptr = '\0';
+
+        /* concatenate filename with pathname */
+        strncat(pathname, "/", pathmax - length);
+        strncat(pathname, dir->d_name, pathmax - length);
+
+        if (lstat(pathname, &stbuf) < 0)
+            err_sys("lstat error: %s", pathname);
+
+        fcategorise(pathname, stbuf.st_mode);
+
+        if (S_ISDIR(stbuf.st_mode))
+            statistic(pathname, strlen(pathname));
+    }
+
+    closedir(dp);
 }
 
 /**
@@ -30,31 +94,21 @@ static void myFtw(char *pathname)
  */
 static void fcategorise(char *filename, const unsigned int st_mode)
 {
-    switch (st_mode) {
-        case S_ISREG(st_mode):
-            n_rfile++;
-            break;
-        case S_ISDIR(st_mode):
-            n_dfile++;
-            break;
-        case S_ISBLK(st_mode):
-            n_bfile++;
-            break;
-        case S_ISCHR(st_mode):
-            n_cfile++;
-            break;
-        case S_ISFIFO(st_mode):
-            n_pfile++;
-            break;
-        case S_ISSOCK(st_mode):
-            n_sfile++;
-            break;
-        case S_ISLNK(st_mode):
-            n_lfile++;
-            break;
-        default:
-            fprintf(stderr, "Unknown file: %s\n", filename);
-            break;
-    }
+    if (S_ISREG(st_mode))
+        n_rfile++;
+    else if (S_ISDIR(st_mode))
+        n_dfile++;
+    else if (S_ISBLK(st_mode))
+        n_bfile++;
+    else if (S_ISCHR(st_mode))
+        n_cfile++;
+    else if (S_ISFIFO(st_mode))
+        n_pfile++;
+    else if (S_ISSOCK(st_mode))
+        n_sfile++;
+    else if (S_ISLNK(st_mode))
+        n_lfile++;
+    else
+        fprintf(stderr, "Unknown file %s\n", filename);
 }
 
--- object.c
+++ object.c
@@ -1,29 +1,114 @@
-#include "./lib/common.h"
+#include "common.h"
+
+static void myftw(char *);
+static void statistic(char *pathname, int length);
+static void fcategorise(char *filename, const unsigned int st_mode);
+
+static int n_rfile;  // number of regular file count.
+static int n_dfile;  // number of directory count.
+static int n_bfile;  // number of block special file count.
+static int n_cfile;  // number of character special file count.
+static int n_pfile;  // number of named pipe count.
+static int n_sfile;  // number of socket count.
+static int n_lfile;  // number of symbolic link count.
+
 
 int main(int argc, char *argv[])
 {
-    int fd;
-    struct stat st;
-    struct timespec times[2];
-
     if (argc != 2)
-        err_quit("Usage: %s <filename>", argv[0]);
+        err_quit("Usage: %s <pathname>", argv[0]);
 
-    if (stat(argv[1], &st) < 0) 
-        err_sys("stat error");
+    myftw(argv[1]);
 
-    if ((fd = open(argv[1], O_WRONLY | O_TRUNC)) < 0)
-        err_sys("open file %s error", argv[1]);
+    printf("n_rfile: %d\n", n_rfile);
+    printf("n_dfile: %d\n", n_dfile);
+    printf("n_bfile: %d\n", n_bfile);
+    printf("n_cfile: %d\n", n_cfile);
+    printf("n_pfile: %d\n", n_pfile);
+    printf("n_sfile: %d\n", n_sfile);
+    printf("n_lfile: %d\n", n_lfile);
+    printf("  total: %d\n", n_rfile + n_dfile + n_bfile + n_cfile + n_pfile + n_sfile + n_lfile);
+}
 
-    /* reset access time */
-    times[0] = st.st_atim;
 
-    /* reset modification time */
-    times[1] = st.st_mtim;
+static size_t pathmax;
+static char *pathnamebuf;
 
-    if (futimens(fd, times) < 0)
-        err_sys("futimesn error");
 
-    exit(0);
+static void myftw(char *pathname)
+{
+    pathnamebuf = path_alloc(&pathmax);
+    strncpy(pathnamebuf, pathname, pathmax);
+    strcat(pathnamebuf, "\0");
+    
+    statistic(pathnamebuf, strlen(pathnamebuf));
+}
+
+
+/**
+ * Recursive dealing file and directory. 
+ */
+static void statistic(char *pathname, int length)
+{
+    DIR *dp;
+    char *ptr;
+    struct stat stbuf;
+    struct dirent *dir;
+    
+    if (lstat(pathname, &stbuf) < 0)
+        err_sys("lstat for %s error", pathname);
+
+    if (!S_ISDIR(stbuf.st_mode))
+        return;
+
+    if ((dp = opendir(pathname)) == NULL)
+        err_sys("opendir path %s error", pathname);
+
+    /* read dir entries */
+    while ((dir = readdir(dp)) != NULL) {
+        if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
+            continue;
+
+        ptr = pathname + length + 1;
+
+        *ptr = '\0';
+
+        /* concatenate filename with pathname */
+        strncat(pathname, "/", pathmax - length);
+        strncat(pathname, dir->d_name, pathmax - length);
+
+        if (lstat(pathname, &stbuf) < 0)
+            err_sys("lstat error: %s", pathname);
+
+        fcategorise(pathname, stbuf.st_mode);
+
+        if (S_ISDIR(stbuf.st_mode))
+            statistic(pathname, strlen(pathname));
+    }
+
+    closedir(dp);
+}
+
+/**
+ * file type counter.
+ */
+static void fcategorise(char *filename, const unsigned int st_mode)
+{
+    if (S_ISREG(st_mode))
+        n_rfile++;
+    else if (S_ISDIR(st_mode))
+        n_dfile++;
+    else if (S_ISBLK(st_mode))
+        n_bfile++;
+    else if (S_ISCHR(st_mode))
+        n_cfile++;
+    else if (S_ISFIFO(st_mode))
+        n_pfile++;
+    else if (S_ISSOCK(st_mode))
+        n_sfile++;
+    else if (S_ISLNK(st_mode))
+        n_lfile++;
+    else
+        fprintf(stderr, "Unknown file %s\n", filename);
 }
 

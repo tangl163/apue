4d99541f5b79c6ba7ba34e0aac3baeaa2be76b37
tangl163 <cnjsw001@gmail.com>
2017-05-30 18:07:47 +0800

    code

---
+++ 4.17exercise.c
@@ -0,0 +1,20 @@
+#include "common.h"
+
+#define RWRWRW (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+
+int main(int argc, char *argv[])
+{
+    int fd;
+
+    unlink(argv[1]);
+    if ((fd = creat(argv[1], RWRWRW)) < 0)
+        err_sys("creat error for: %s", argv[1]);
+
+    if (write(fd, "nihao", 5) != 5)
+        err_sys("write error");
+
+    close(fd);
+
+    exit(0);
+}
+

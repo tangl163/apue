1c573b1a23f30cbfa8b0b6d657e777e93255d8b6
tangl163 <cnjsw001@gmail.com>
2017-08-10 20:55:02 +0800

    get RLIMIT_NOFILE

---
+++ example7.11.c
@@ -0,0 +1,16 @@
+#include "common.h"
+#include <sys/resource.h>
+
+int main(void)
+{
+    struct rlimit rlptr;
+
+    if (getrlimit(RLIMIT_NOFILE, &rlptr) < 0)
+        err_sys("getrlimit error");
+
+    printf("soft limit: %ld\n", rlptr.rlim_cur);
+    printf("hard limit: %ld\n", rlptr.rlim_max);
+
+    exit(0);
+}
+

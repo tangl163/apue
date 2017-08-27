66f7a16dca05b352e09af0e785a01545ad582a06
tangl163 <cnjsw001@gmail.com>
2017-08-27 19:46:40 +0800

    test privileges

---
+++ example8.10.c
@@ -0,0 +1,20 @@
+#include "common.h"
+
+/**
+ * When a process with super privileges `exec` a `setuid`
+ * executable (doesn't belong to `root`), it will lose the super priveleges.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        err_quit("Usage: %s <filenam>", argv[0]);
+
+    printf("uid=%ld, gid=%ld, euid=%ld, egid=%ld\n",
+            (long)getuid(), (long)getgid, (long)geteuid(), (long)getegid());
+
+    if (unlink(argv[1]) < 0)
+        err_sys("unlink error");
+
+    exit(0);
+}
+

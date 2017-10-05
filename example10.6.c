6ac298f9ef2585272609330c011e28a8a86942a2
tangl163 <cnjsw001@gmail.com>
2017-10-06 00:22:54 +0800

    program seem to be blocked in the sig_alarm handler

---
+++ example10.6.c
@@ -0,0 +1,38 @@
+#include "common.h"
+#include <pwd.h>
+#include <signal.h>
+
+static void sig_alarm(int);
+
+int
+main(void)
+{
+    struct passwd *ptr;
+
+    if (signal(SIGALRM, sig_alarm) == SIG_ERR)
+        err_sys("install signal for SIGALRM error");
+
+    alarm(1);
+
+    while (TRUE) {
+        if ((ptr = getpwnam("paul")) == NULL)
+            err_sys("getpwnam erro");
+
+        if (strcmp(ptr->pw_name, "paul") != 0) 
+            printf("username: %s, passwd: %s\n", ptr->pw_name, ptr->pw_passwd);
+    }
+}
+
+static void
+sig_alarm(int signo)
+{
+    struct passwd *ptr;
+
+    printf("sigal caught: %d\n", signo);
+
+    if ((ptr = getpwnam("root")) == NULL)
+        err_sys("getpwnam(root) error");
+    
+    alarm(1);
+}
+

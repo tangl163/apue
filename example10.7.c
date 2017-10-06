58b28aebd2cf528da86cf83c3da7c305335c2136
tangl163 <cnjsw001@gmail.com>
2017-10-06 23:46:46 +0800

    Wait won't be the same if we ignore SIGCHLD.
    Wait won't be the same if we ignore SIGCHLD specifically.
    The calling process will block until all its children
    have terminated, and then wait returns −1 with errno set to ECHILD.

--- example10.7.c
+++ example10.7.c
@@ -2,33 +2,31 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-static void sig_chld(int);
+//static void sig_chld(int);
 
 int
 main(void)
 {
     pid_t pid;
 
+    if (signal(SIGCHLD, SIG_IGN) == SIG_ERR)
+        err_sys("install signal handle for SIGCHLD error");
+
     if ((pid = fork()) < 0) {
         err_sys("fork error");
     } else if (pid == 0) {
-        _exit(0);
+        _exit(0);    // first child
     }
 
-    sleep(2);
-    
-    if (signal(SIGCHLD, sig_chld) == SIG_ERR)
-        err_sys("install signal handle for SIGCHLD error");
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        sleep(10);    // second child
+    }
+
+    if (wait(NULL) < 0)
+        err_sys("wait error");
 
     exit(0);
-}
-
-static void
-sig_chld(int signo)
-{
-    printf("Caught signal: %d\n", signo);
-
-    if (waitpid(-1, NULL, 0) < 0)
-        err_sys("waitpid error");
 }
 

cadc5eeb5d0405e3c4103d230d7838f79f058e78
tangl163 <cnjsw001@gmail.com>
2017-11-13 11:29:13 +0800

    Testing SA_RESTART flag when using sigaction
    System call won't be restarted if we don't specify SA_RESTART flag
    when arrange signal dispositions using sigaction.

--- example10.15.c
+++ example10.15.c
@@ -1,15 +1,38 @@
 #include "common.h"
 #include <signal.h>
+#include <sys/wait.h>
 
 static void sig_int(int);
 
 int
 main(void)
 {
-    if (signal(SIGINT, sig_int) == SIG_ERR)
-        err_sys("signal error");
+    pid_t pid;
+    struct sigaction act;
 
-    pause();
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+
+        /* We ignore SIGINT in the child. */
+        if (signal(SIGINT, SIG_IGN) == SIG_ERR)
+            err_sys("signal error");
+
+        sleep(15);
+        exit(0);
+    }
+    
+    act.sa_flags = 0;
+    //act.sa_flags |= SA_RESTART;
+    act.sa_handler = sig_int;
+    sigemptyset(&act.sa_mask);
+
+    if (sigaction(SIGINT, &act, NULL) < 0)
+        err_sys("sigaction error");
+
+    /* We test if waitpid could be interrupted or not. */
+    if (waitpid(pid, NULL, 0) < 0)
+        err_sys("waitpid error");
 
     exit(0);
 }

9632bca1e1ad085e53869e8b1d0786bd234fe4d5
tangl163 <cnjsw001@gmail.com>
2018-01-22 22:14:11 +0800

    implement pthread_barrier_wait
    Implement pthread_barrier_wait through pthread_mutex_t and
    pthread_cond_t

--- 11.5exercise.c
+++ 11.5exercise.c
@@ -8,12 +8,52 @@ struct my_barrier_t {
     pthread_cond_t   b_cond;
 };
 
+static void *start_routine(void *arg);
 static int my_barrier_init(struct my_barrier_t *barrier, unsigned int count);
 static int my_barrier_wait(struct my_barrier_t *barrier);
 
+static struct my_barrier_t barrier;
+
+/**
+ * 11.5 What synchronization primitives would you need to implement a barrier?
+ * Provide an implementation of the pthread_barrier_wait function.
+ */
 int
 main(void)
 {
+    long i;
+    int err, ret;
+    pthread_t tid;
+
+    my_barrier_init(&barrier, 4);
+
+    for (i = 0; i < 3; i++) {
+        err = pthread_create(&tid, NULL, start_routine, (void *)i);
+        if (err != 0) {
+            printf("pthread_create error: %s\n", strerror(err));
+            exit(0);
+        }
+    }
+
+    ret = my_barrier_wait(&barrier);
+
+    sleep(2);
+    printf("ret: %d\n", ret);
+
+    exit(0);
+}
+
+static void *
+start_routine(void *arg)
+{
+    int ret;
+
+    ret = my_barrier_wait(&barrier);
+
+    printf("I'm thread: %lu with arg: %ld ret: %d\n",
+            (unsigned long)pthread_self(), (long)(arg), ret);
+
+    return (void *)0;
 }
 
 static int
@@ -38,7 +78,18 @@ my_barrier_init(struct my_barrier_t *barrier, unsigned int count)
 static int
 my_barrier_wait(struct my_barrier_t *barrier)
 {
-    pthread_cond_wait(&barrier->b_cond, &barrier->b_lock);
+    pthread_mutex_lock(&barrier->b_lock);
+    barrier->b_nwait++;
 
+    if (barrier->b_nwait == barrier->b_count) {
+        pthread_mutex_unlock(&barrier->b_lock);
+        pthread_cond_broadcast(&barrier->b_cond);
+        return PTHREAD_BARRIER_SERIAL_THREAD;
+    }
+
+    pthread_cond_wait(&barrier->b_cond, &barrier->b_lock);
+    pthread_mutex_unlock(&barrier->b_lock);
+
+    return 0;
 }
 

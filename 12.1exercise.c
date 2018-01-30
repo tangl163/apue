5446f505818fd84fd9ef4c90b6fc2eb6685f250a
tangl163 <cnjsw001@gmail.com>
2018-01-30 11:35:42 +0800

    Exercise 12.1

---
+++ 12.1exercise.c
@@ -0,0 +1,72 @@
+#include "common.h"
+#include <pthread.h>
+
+static void prepare(void);
+static void parent(void);
+static void child(void);
+static void * start_rt(void *arg);
+
+static pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+
+int
+main(void)
+{
+    int err;
+    pid_t pid;
+    pthread_t tid;
+
+    err = pthread_atfork(prepare, parent, child);
+    if (err != 0)
+        err_exit(err, "pthread_atfork error");
+
+    err = pthread_create(&tid, NULL, start_rt, NULL);
+    if (err != 0)
+        err_exit(err, "pthread_create error");
+
+    sleep(2);
+    printf("parent is about to fork\n");
+
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        printf("child returned from fork\n");
+    } else {
+        printf("parent returned from fork\n");
+    }
+
+    exit(0);
+}
+
+static void *
+start_rt(void *arg)
+{
+    printf("In thread\n");
+    pause();
+    return arg;
+}
+
+static void
+prepare(void)
+{
+    printf("Prepare locking\n");
+    pthread_mutex_lock(&lock1);
+    pthread_mutex_lock(&lock2);
+}
+
+static void
+parent(void)
+{
+    printf("Parent unlocking\n");
+    pthread_mutex_unlock(&lock1);
+    pthread_mutex_unlock(&lock2);
+}
+
+static void
+child(void)
+{
+    printf("Child unlocking\n");
+    pthread_mutex_unlock(&lock1);
+    pthread_mutex_unlock(&lock2);
+}
+
